ab8f2f10a06e2728fbcc69f2c67a85adf2db182a
Kohei Nishimura <koh.oct.un@gmail.com>
2018-02-24 22:30:27 +0900

    rust study

---
+++ hello_world/target/debug/deps/hello_world-bac7bed82dba286e.d
@@ -0,0 +1,5 @@
+/Users/k/Src/rust_study/hello_world/target/debug/deps/hello_world-bac7bed82dba286e: src/main.rs
+
+/Users/k/Src/rust_study/hello_world/target/debug/deps/hello_world-bac7bed82dba286e.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/k/Src/rust_study/hello_world/target/debug/hello_world.dSYM: /Users/k/Src/rust_study/hello_world/src/main.rs
